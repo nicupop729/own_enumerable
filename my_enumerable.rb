229c13e16fa015c725dfb5c34cc910abff301389
Nicolae Pop <nicupop729@gmail.com>
2022-01-20 19:08:19 -0300

    fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,22 +1,23 @@
 module MyEnumerable
   def all?
-    each do |elem| return false unless yield(elem)
+    each do |elem|
+      return false unless yield(elem)
     end
     true
   end
 
   def any?
-    each do |elem| return true if yield(elem)
+    each do |elem|
+      return true if yield(elem)
     end
     false
   end
 
   def filter
     array_filter = []
-      each do  |elem| 
-        array_filter << (elem) if yield (elem)
-      end
-      array_filter
+    each do |elem|
+      array_filter << (elem) if yield elem
+    end
+    array_filter
   end
 end
-
--- my_list.rb
+++ my_list.rb
@@ -1,17 +1,14 @@
 require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
-  def initialize (*list)
+  def initialize(*list)
     @list = list
   end
 
   def each
     @list.each { |item| yield(item) if block_given? }
-
   end
-
 end
 
 list = MyList.new(1, 2, 3, 4)
-print list.filter { |e| e.even?}
-
+print list.filter(&:even?)
